985f9288a861ecdb5d7ae13868216b48e3df1d7e
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-09-23 15:35:10 -0600

    Guía I - Entity Framework (finalizada)

---
+++ EFDemo/EFDemo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EFDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnObtenerTodo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(30, 34);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(732, 286);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // btnObtenerTodo
+            // 
+            this.btnObtenerTodo.Location = new System.Drawing.Point(361, 360);
+            this.btnObtenerTodo.Name = "btnObtenerTodo";
+            this.btnObtenerTodo.Size = new System.Drawing.Size(95, 24);
+            this.btnObtenerTodo.TabIndex = 1;
+            this.btnObtenerTodo.Text = "Cargar todo";
+            this.btnObtenerTodo.UseVisualStyleBackColor = true;
+            this.btnObtenerTodo.Click += new System.EventHandler(this.btnObtenerTodo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 428);
+            this.Controls.Add(this.btnObtenerTodo);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnObtenerTodo;
+    }
+}
+
