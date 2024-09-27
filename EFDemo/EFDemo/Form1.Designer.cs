72f2c4a42fe37cec7e68306b7e6469c26980b0f1
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-09-27 14:27:43 -0600

    Guía III - Insertar un cliente (finalizada)

--- EFDemo/EFDemo/Form1.Designer.cs
+++ EFDemo/EFDemo/Form1.Designer.cs
@@ -33,6 +33,17 @@
             this.txtObtenerPorId = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnObtenerPorId = new System.Windows.Forms.Button();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContactTitle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,12 +52,12 @@
             this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCustomers.Location = new System.Drawing.Point(30, 34);
             this.dgvCustomers.Name = "dgvCustomers";
-            this.dgvCustomers.Size = new System.Drawing.Size(732, 286);
+            this.dgvCustomers.Size = new System.Drawing.Size(732, 142);
             this.dgvCustomers.TabIndex = 0;
             // 
             // btnObtenerTodo
             // 
-            this.btnObtenerTodo.Location = new System.Drawing.Point(361, 360);
+            this.btnObtenerTodo.Location = new System.Drawing.Point(363, 203);
             this.btnObtenerTodo.Name = "btnObtenerTodo";
             this.btnObtenerTodo.Size = new System.Drawing.Size(95, 24);
             this.btnObtenerTodo.TabIndex = 1;
@@ -56,7 +67,7 @@
             // 
             // txtObtenerPorId
             // 
-            this.txtObtenerPorId.Location = new System.Drawing.Point(99, 363);
+            this.txtObtenerPorId.Location = new System.Drawing.Point(101, 206);
             this.txtObtenerPorId.Name = "txtObtenerPorId";
             this.txtObtenerPorId.Size = new System.Drawing.Size(100, 20);
             this.txtObtenerPorId.TabIndex = 7;
@@ -64,7 +75,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 366);
+            this.label1.Location = new System.Drawing.Point(33, 209);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 13);
             this.label1.TabIndex = 6;
@@ -72,7 +83,7 @@
             // 
             // btnObtenerPorId
             // 
-            this.btnObtenerPorId.Location = new System.Drawing.Point(218, 360);
+            this.btnObtenerPorId.Location = new System.Drawing.Point(220, 203);
             this.btnObtenerPorId.Name = "btnObtenerPorId";
             this.btnObtenerPorId.Size = new System.Drawing.Size(95, 24);
             this.btnObtenerPorId.TabIndex = 5;
@@ -80,11 +91,112 @@
             this.btnObtenerPorId.UseVisualStyleBackColor = true;
             this.btnObtenerPorId.Click += new System.EventHandler(this.btnObtenerPorId_Click);
             // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(187, 267);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerID.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IDCustomer";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(187, 293);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(100, 20);
+            this.txtCompanyName.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "CompanyName";
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(187, 319);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(100, 20);
+            this.txtContactName.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "ContactName";
+            // 
+            // txtContactTitle
+            // 
+            this.txtContactTitle.Location = new System.Drawing.Point(187, 345);
+            this.txtContactTitle.Name = "txtContactTitle";
+            this.txtContactTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtContactTitle.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(117, 348);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "ContactTitle";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(187, 371);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(136, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Address";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(336, 316);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(95, 24);
+            this.btnInsertar.TabIndex = 18;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 428);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtContactTitle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtObtenerPorId);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnObtenerPorId);
@@ -105,6 +217,17 @@
         private System.Windows.Forms.TextBox txtObtenerPorId;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnObtenerPorId;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContactTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnInsertar;
     }
 }
 
